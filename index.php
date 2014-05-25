8f4502b22c21b4fe799c0800ceeaf9a069d31582
Blake Howe <howe.bobby@gmail.com>
2014-05-25 15:36:26 -0500

    basic twitter calls working

--- index.php
+++ index.php
@@ -29,17 +29,42 @@ List all suggestions with popularity score
 error_reporting(E_ALL);
 require_once('patent_search/PatentSearch.php');
 
-$patent_query = "King kong";
+$query = "King kong";
 $google_api_key = "ENTER YOUR API KEY HERE";
 $starting_result = '1';
 
+//get the number of patents with search term
+
+
 //of course this email should trigger it  
-$psearch= new PatentSearch($patent_query, $google_api_key,$starting_result);
+$psearch= new PatentSearch($query, $google_api_key,$starting_result);
 $data =$psearch->get_data();
 	
 if ($data){
 	$psearch->writeSearchResults();
 }
+
+
+
+// from https://dev.twitter.com/docs/auth/application-only-auth
+$consumer_key = '';
+$consumer_secret = '';
+#https://dev.twitter.com/docs/auth/application-only-auth
+#remember bearer tokens never expire
+$bearer_token = "";
+
+
+
+//Init curl
+$request = curl_init();
+curl_setopt($request, CURLOPT_SSLVERSION, 3);
+curl_setopt($request, CURLOPT_URL, 'https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=block76&count=100');
+curl_setopt($request, CURLOPT_HTTPHEADER, array('Authorization: Bearer '.$bearer_token));
+curl_setopt($request, CURLOPT_RETURNTRANSFER, true);
+$result = json_decode($content = curl_exec($request)); curl_close($request);
+
+var_dump($result)
+
 
 
 ?>
